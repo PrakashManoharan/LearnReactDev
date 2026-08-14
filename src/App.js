6008f2dca24a48bdccad20844137409a32e683fb
PRAX <prakash.manoharan@outlook.com>
2026-08-15 00:01:13 +1000

    feat: conditionally rendering jsx

--- src/App.js
+++ src/App.js
@@ -1,63 +1,76 @@
-import { getImageUrl } from "./utils.js";
+//Logical AND operator (&&) 
+//Another common shortcut you’ll encounter is the JavaScript logical AND (&&) operator. 
+// Inside React components, it often comes up when you want to render some JSX when the condition
+// is true, or render nothing otherwise. With &&, you could conditionally render 
+// the checkmark only if isPacked is true:
+// return (
+//   <li className="item">
+//     {name} {isPacked && '✅'}
+//   </li>
+// );
 
-function Profile({
-  name,
-  imageId,
-  profession,
-  awards,
-  discovery,
-  imageSize = 70,
-}) {
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {name} {isPacked && '✅'}
+//     </li>
+//   );
+// }
+
+// Like before, this works not only for text, but for arbitrary JSX too:
+
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {isPacked ? (
+//         <del>
+//           {name + ' ✅'}
+//         </del>
+//       ) : (
+//         name
+//       )}
+//     </li>
+//   );
+// }
+
+
+// Curly braces open the “window into JavaScript”.
+// Embed the variable with curly braces in the returned JSX tree, 
+// nesting the previously calculated expression inside of JSX:
+
+{/* <li className="item">
+  {itemContent}
+</li> */}
+function Item({ name, isPacked }) {
+  let itemContent = name;
+  if (isPacked) {
+    itemContent = name + " ✅";
+  }
   return (
-    <section className="profile">
-      <h2>{name}</h2>
-      <img
-        className="avatar"
-        src={getImageUrl(imageId)}
-        alt={name}
-        width={imageSize}
-        height={imageSize}
-      />
-      <ul>
-        <li>
-          <b>Profession:</b>
-          {profession}
-        </li>
-        <li>
-          <b>Awards: {awards.length}</b>({awards.join(", ")})
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {discovery}
-        </li>
-      </ul>
-    </section>
+    <li className="item">
+      {itemContent}
+    </li>
   );
 }
 
-export default function Gallery() {
+export default function PackingList() {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        imageId="szV5sdG"
-        name="Maria Skłodowska-Curie"
-        profession="physicist and chemist"
-        discovery="polonium (chemical element)"
-        awards={[
-          "Nobel Prize in Physics",
-          "Nobel Prize in Chemistry",
-          "Davy Medal",
-          "Matteucci Medal",
-        ]}
-      />
-      <Profile
-        imageId="YfeOqp2"
-        name="Katsuko Saruhashi"
-        profession="geochemist"
-        discovery="a method for measuring carbon dioxide in seawater"
-        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
-      />
-    </div>
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
   );
 }
